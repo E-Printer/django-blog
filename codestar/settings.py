188e92bbcab4a69ddbef37b645249dea6489290a
Edward Printer <edwardprxnter@gmail.com>
2024-11-19 14:15:03 +0000

    secret key added

--- codestar/settings.py
+++ codestar/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-x#9cdlo#367s=74&aoexp9=%uxvex^43$556v*%!)tp@1^-^2^'
+SECRET_KEY = 'yPj005NeF8%.1);}#*|fhYQ)]=MT[&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -88,6 +88,8 @@ WSGI_APPLICATION = 'codestar.wsgi.application'
 DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
+
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
